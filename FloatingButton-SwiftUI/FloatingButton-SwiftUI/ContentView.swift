f52cfff8f48cd72c0bd8ed5eecc68d47e9062437
TotoroQ <totoroq@outlook.com>
2019-08-12 16:50:41 +0800

    Init project files

---
+++ FloatingButton-SwiftUI/FloatingButton-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FloatingButton-SwiftUI
+//
+//  Created by Quentin on 2019/8/12.
+//  Copyright © 2019 Quentin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
